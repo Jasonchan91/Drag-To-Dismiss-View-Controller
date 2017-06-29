fc2255eed5115d4adb83fe6235d5a24b08a179f2
Jason Chan <j.ianchan.j@gmail.com>
2017-06-29 15:06:14 +0800

    Initial Commit

---
+++ Drag to Dismiss/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Drag to Dismiss
+//
+//  Created by Jason Chan on 29/6/17.
+//  Copyright © 2017 Jason Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
